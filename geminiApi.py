0e5700f9e33b2b1a4e0e7363898e236afb0243ac
onuralpArlsn <onuralparslan97@gmail.com>
2025-11-22 13:53:39 +0300

    expalnations

--- geminiApi.py
+++ geminiApi.py
@@ -7,23 +7,26 @@ from dotenv import load_dotenv
 from google import genai
 #python3.12 -m pip install google-genai
 
-# Load environment variables from .env file
+# .env dosyadanı yükle
 load_dotenv()
 
-# Get API key from environment
+# .env dosyadandan özel bilgileri al
 api_key = os.getenv('GEMINI_API_KEY')
 
+# google gemini istemcisini oluştur
 client = genai.Client(api_key=api_key)
 
-# 1) Create a persistent chat
+# 1) Bir chat başlat
 chat = client.chats.create(
     model="gemini-2.5-flash"
 )
 
-# 2) Send messages on the same chat object
+# 2) Chate mesaj at ve cevabı response olarak kaydet 
 response = chat.send_message("Explain how AI works in a few words")
+# response cevabını ekrana yazdır
 print(response.text)
 
-# 3) Send another message; it keeps context
+# 3) Aynı chat üzerinde yeni mesaj at ve cevabı response olarak kaydet 
 response = chat.send_message("Can you make it even shorter?")
+# response cevabını ekrana yazdır
 print(response.text)
